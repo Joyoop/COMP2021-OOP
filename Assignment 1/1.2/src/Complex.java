c96377dafe09a75b840eb2ffa27b1cc52a5b3884
ShingJan <yuanj.shi@hotmail.com>
2016-09-24 21:28:41 +0800

    assignment 1 - part 2 finished!

--- Assignment 1/1.2/src/Complex.java
+++ Assignment 1/1.2/src/Complex.java
@@ -1,31 +1,67 @@
+import java.util.Objects;
+
 public class Complex {
 
-    // Task 3: add the missing fields
+    private final double re;   // the real part
+    private final double im;   // the imaginary part
 
-
-    Complex(double real, double imag){
-        // Task 4: complete the constructor
-
+    // create a new object with the given real and imaginary parts
+    public Complex(double real, double imag) {
+        re = real;
+        im = imag;
     }
 
-    Complex add(Complex other){
-        // Task 4: complete the method
-
+    // return a string representation of the invoking Complex object
+    public String toString() {
+        if (im == 0) return re + "";
+        if (re == 0) return im + "i";
+        if (im <  0) return re + " - " + (-im) + "i";
+        return re + " + " + im + "i";
     }
 
-    Complex multiply(Complex other){
-        // Task 4: complete the method
-
+    // return a new Complex object whose value is (this + b)
+    public Complex plus(Complex b) {
+        Complex a = this;             // invoking object
+        double real = a.re + b.re;
+        double imag = a.im + b.im;
+        return new Complex(real, imag);
     }
 
-    String asString(){
-        // Task 4: complete the method
-
+    // return a new Complex object whose value is (this - b)
+    public Complex minus(Complex b) {
+        Complex a = this;
+        double real = a.re - b.re;
+        double imag = a.im - b.im;
+        return new Complex(real, imag);
     }
 
-    public static void main(String[] args){
-        // Task 5: create Complex objects, add or multiply them, and
-        //         print the results out to check they are correct
+    // return a new Complex object whose value is (this * b)
+    public Complex times(Complex b) {
+        Complex a = this;
+        double real = a.re * b.re - a.im * b.im;
+        double imag = a.re * b.im + a.im * b.re;
+        return new Complex(real, imag);
+    }
+
+
+    // return the real or imaginary part
+    public double re() { return re; }
+    public double im() { return im; }
+
+
+    // sample client for testing
+    public static void main(String[] args) {
+        Complex a = new Complex(5.0, 6.0);
+        Complex b = new Complex(-3.0, 4.0);
+
+        System.out.println("a            = " + a);
+        System.out.println("b            = " + b);
+        System.out.println("Re(a)        = " + a.re());
+        System.out.println("Im(a)        = " + a.im());
+        System.out.println("b + a        = " + b.plus(a));
+        System.out.println("a - b        = " + a.minus(b));
+        System.out.println("a * b        = " + a.times(b));
+        System.out.println("b * a        = " + b.times(a));
 
     }
 
