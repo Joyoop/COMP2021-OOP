1df16607286f5a768790a6c9dc7a3fb187c6efe7
ShingJan <yuanj.shi@hotmail.com>
2016-09-28 23:18:39 +0800

    some update of Complex

--- Assignment 1/1.2/src/Complex.java
+++ Assignment 1/1.2/src/Complex.java
@@ -1,5 +1,3 @@
-import java.util.Objects;
-
 public class Complex {
 
     private final double re;   // the real part
@@ -13,30 +11,25 @@ public class Complex {
 
     // return a string representation of the invoking Complex object
     public String toString() {
-        if (im == 0) return re + "";
-        if (re == 0) return im + "i";
-        if (im <  0) return re + " - " + (-im) + "i";
-        return re + " + " + im + "i";
+        if (im == 0)
+            return re + "";
+        if (re == 0)
+            return im + "i";
+        if (im <  0)
+            return re + " - " + (-im) + "i";
+            return re + " + " + im + "i";
     }
 
     // return a new Complex object whose value is (this + b)
-    public Complex plus(Complex b) {
+    public Complex add(Complex b) {
         Complex a = this;             // invoking object
         double real = a.re + b.re;
         double imag = a.im + b.im;
         return new Complex(real, imag);
     }
 
-    // return a new Complex object whose value is (this - b)
-    public Complex minus(Complex b) {
-        Complex a = this;
-        double real = a.re - b.re;
-        double imag = a.im - b.im;
-        return new Complex(real, imag);
-    }
-
     // return a new Complex object whose value is (this * b)
-    public Complex times(Complex b) {
+    public Complex multiply(Complex b) {
         Complex a = this;
         double real = a.re * b.re - a.im * b.im;
         double imag = a.re * b.im + a.im * b.re;
@@ -51,17 +44,16 @@ public class Complex {
 
     // sample client for testing
     public static void main(String[] args) {
-        Complex a = new Complex(5.0, 6.0);
+        Complex a = new Complex(5.0, -6.0);
         Complex b = new Complex(-3.0, 4.0);
 
-        System.out.println("a            = " + a);
-        System.out.println("b            = " + b);
-        System.out.println("Re(a)        = " + a.re());
-        System.out.println("Im(a)        = " + a.im());
-        System.out.println("b + a        = " + b.plus(a));
-        System.out.println("a - b        = " + a.minus(b));
-        System.out.println("a * b        = " + a.times(b));
-        System.out.println("b * a        = " + b.times(a));
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("Real(a) = " + a.re());
+        System.out.println("Imaginary(a) = " + a.im());
+        System.out.println("a + b = " + a.add(b));
+        System.out.println("a * b = " + a.multiply(b));
+        System.out.println("b * a = " + b.multiply(a));
 
     }
 
