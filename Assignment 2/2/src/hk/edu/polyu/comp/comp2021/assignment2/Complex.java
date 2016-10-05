555334c730f49eb3eff31028991999d886aea152
ShingJan <yuanj.shi@hotmail.com>
2016-10-05 22:48:28 +0800

    assignment 2 Update

---
+++ Assignment 2/2/src/hk/edu/polyu/comp/comp2021/assignment2/Complex.java
@@ -0,0 +1,38 @@
+package hk.edu.polyu.comp.comp2021.assignment2;
+
+public class Complex {
+
+    // Task 4: add the missing fields
+
+
+    public Complex(Rational real, Rational imag){
+        // Assume 'real' and 'imag' are not null
+        // Task 5: complete the constructor
+
+    }
+
+    public Complex add(Complex other){
+        // Assume 'other' is not null
+        // Task 5: complete the method
+
+    }
+
+    public Complex multiply(Complex other){
+        // Assume 'other' is not null
+        // Task 5: complete the method
+
+    }
+
+    public String asString(){
+        // Task 5: complete the method
+
+    }
+
+    public static void main(String[] args){
+        // Task 6: create Complex objects, add or multiply them, and
+        //         print the results out to check they are correct
+
+    }
+
+
+}
---
+++ Assignment 2/2/src/hk/edu/polyu/comp/comp2021/assignment2/Rational.java
@@ -0,0 +1,36 @@
+package hk.edu.polyu.comp.comp2021.assignment2;
+
+public class Rational {
+    // Task 1: add the missing fields
+
+
+    public Rational(int numerator, int denominator){
+        // Assume 'denominator' is not 0;
+        // Task 2: complete the constructor
+
+    }
+
+    public Rational add(Rational other){
+        // Assume 'other' is not null
+        // Task 2: complete the method
+
+    }
+
+    public Rational multiply(Rational other){
+        // Assume 'other' is not null
+        // Task 2: complete the method
+
+    }
+
+    public String asString(){
+        // Task 2: complete the method
+
+    }
+
+    public static void main(String[] args){
+        // Task 3: create Rational objects, add or multiply them, and
+        //         print the results out to check they are correct
+
+    }
+
+}
