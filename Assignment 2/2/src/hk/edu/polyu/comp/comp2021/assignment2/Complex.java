c12bd6a2b354f78dbde9d2fff028c54ea8e1363d
ShingJan <yuanj.shi@hotmail.com>
2016-10-05 22:59:23 +0800

    something need to figure out

--- Assignment 2/2/src/hk/edu/polyu/comp/comp2021/assignment2/Complex.java
+++ Assignment 2/2/src/hk/edu/polyu/comp/comp2021/assignment2/Complex.java
@@ -3,34 +3,60 @@ package hk.edu.polyu.comp.comp2021.assignment2;
 public class Complex {
 
     // Task 4: add the missing fields
+    private final double re;   // the real part
+    private final double im;   // the imaginary part
 
-
-    public Complex(Rational real, Rational imag){
-        // Assume 'real' and 'imag' are not null
-        // Task 5: complete the constructor
-
+    // create a new object with the given real and imaginary parts
+    public Complex(double real, double imag) {
+        re = real;
+        im = imag;
     }
 
-    public Complex add(Complex other){
-        // Assume 'other' is not null
-        // Task 5: complete the method
-
+    // return a string representation of the invoking Complex object
+    public String asString() {
+        if (im == 0)
+            return re + "";
+        if (re == 0)
+            return im + "i";
+        if (im <  0)
+            return re + " - " + (-im) + "i";
+        return re + " + " + im + "i";
     }
 
-    public Complex multiply(Complex other){
-        // Assume 'other' is not null
-        // Task 5: complete the method
-
+    // return a new Complex object whose value is (this + b)
+    public Complex add(Complex b) {
+        Complex a = this;             // invoking object
+        double real = a.re + b.re;
+        double imag = a.im + b.im;
+        return new Complex(real, imag);
     }
 
-    public String asString(){
-        // Task 5: complete the method
-
+    // return a new Complex object whose value is (this * b)
+    public Complex multiply(Complex b) {
+        Complex a = this;
+        double real = a.re * b.re - a.im * b.im;
+        double imag = a.re * b.im + a.im * b.re;
+        return new Complex(real, imag);
     }
 
-    public static void main(String[] args){
-        // Task 6: create Complex objects, add or multiply them, and
-        //         print the results out to check they are correct
+
+    // return the real or imaginary part
+    public double re() { return re; }
+    public double im() { return im; }
+
+
+    // sample client for testing
+    public static void main(String[] args) {
+        Complex a = new Complex(5.0, -6.0);
+        Complex b = new Complex(-3.0, 4.0);
+
+        System.out.println("a = " + a.asString());
+        System.out.println("b = " + b.asString());
+        System.out.println("Real(a) = " + a.re());
+        System.out.println("Imaginary(b) = " + b.im());
+        System.out.println("a + b = " + a.add(b).asString());
+        System.out.println("a * b = " + a.multiply(b).asString());
+        System.out.println("b * a = " + b.multiply(a).asString());
 
     }
 
--- Assignment 2/2/src/hk/edu/polyu/comp/comp2021/assignment2/Rational.java
+++ Assignment 2/2/src/hk/edu/polyu/comp/comp2021/assignment2/Rational.java
@@ -2,28 +2,57 @@ package hk.edu.polyu.comp.comp2021.assignment2;
 
 public class Rational {
     // Task 1: add the missing fields
-
+    private int numerator;
+    private int denominator;
 
     public Rational(int numerator, int denominator){
         // Assume 'denominator' is not 0;
         // Task 2: complete the constructor
+        numerator = 0;
+        denominator = 1;
+    }
+
+    public Rational add(Rational b){
+        // Assume 'other' is not null
+        // Task 2: complete the method
+        Rational a = this;
+
+        // special cases
+        if (a.compareTo(zero) == 0) return b;
+        if (b.compareTo(zero) == 0) return a;
+
+        // Find gcd of numerators and denominators
+        int f = gcd(a.num, b.num);
+        int g = gcd(a.den, b.den);
+
+        // add cross-product terms for numerator
+        Rational s = new Rational((a.num / f) * (b.den / g) + (b.num / f) * (a.den / g),
+                lcm(a.den, b.den));
+
+        // multiply back in
+        s.num *= f;
+        return s;
 
     }
 
-    public Rational add(Rational other){
+    public Rational multiply(Rational b){
         // Assume 'other' is not null
         // Task 2: complete the method
+        Rational a = this;
 
-    }
-
-    public Rational multiply(Rational other){
-        // Assume 'other' is not null
-        // Task 2: complete the method
+        // reduce p1/q2 and p2/q1, then multiply, where a = p1/q1 and b = p2/q2
+        Rational c = new Rational(a.numerator, b.denominator);
+        Rational d = new Rational(b.numerator, a.denominator);
+        return new Rational(c.numerator * d.numerator, c.denominator * d.denominator);
 
     }
 
     public String asString(){
         // Task 2: complete the method
+        if (denominator == 1)
+            return numerator + "";
+        else
+            return numerator + "/" + denominator;
 
     }
 
