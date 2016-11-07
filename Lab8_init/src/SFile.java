c4d732c9148b43cb091bae048b3376bfe0986aaa
ShingJan <yuanj.shi@hotmail.com>
2016-11-07 12:29:37 +0800

    File class finished

--- Lab8_init/src/SFile.java
+++ Lab8_init/src/SFile.java
@@ -13,7 +13,9 @@ public class SFile {
     }
 
     // create your own constructor
-    SFile(arg1 or more) {
+    SFile(String filename, Vector<String> contents) {
+        this.filename = filename;
+        this.contents = contents;
     }
 
     /**
@@ -21,52 +23,75 @@ public class SFile {
      *  @param filename input filename for new file
      */
     public void create(String filename) {
+        this.filename = filename;
     }
 
     // overload create with two params
-    // sz is the initial size of contents
+        // sz is the initial size of contents
     public void create(String filename, int sz) {
         // your code here
+        this.contents.setSize(sz);
+        this.filename = filename;
     }
 
     public void addContent(String content) {
         // your code here
         // add content after the end of current file
+        this.contents.add(content);
     }
 
     public void setFileSize(int sz) {
         // your code here
         // reset the size of current file
+        this.contents.setSize(sz);
     }
 
     public void setFileName(String filename) {
         // your code here
         // rename the current file
+        this.filename = filename;
     }
 
     // put your own comments here (javadoc style)
+    /**
+     * return the size of current file
+     * @return filesize size of current file in int
+     */
     public int getFileSize() {
         // your code here
+        return this.contents.size();
     }
 
     // put your own comments here
     public String getFileName() {
         // your code here
+        return this.filename;
     }
 
     public String getContent(int contentIndex) {
         // your code here
         // the contentIndex should be smaller than max size of
         // current file
+        if(contentIndex <= this.contents.size()){
+            return this.contents.get(contentIndex);
+        }else{
+            return null;
+        }
     }
 
     public void copyTo(SFile rhs) {
         // your code here
+        rhs = new SFile(this.filename,this.contents);
     }
 
     // put your own comments here
     public void swapContents(int index0, int index1) {
         // your code here
+        Vector<String> temp1 = this.contents;
+        String temp = temp1.get(index0);
+        temp1.set(index0,temp1.get(index1));
+        temp1.set(index1,temp);
+
     }
 
     // put your own comments here
@@ -74,13 +99,19 @@ public class SFile {
         // your code here
         // use if to delete all contents (e.g. contentIndex == -1
         // clear all).
+        if(this.contents.get(contentIndex) != null){
+            this.contents.remove(contentIndex);
+        }
     }
 
     public void deleteAll() {
         // your code here
         // delete all things
+        if(!this.contents.isEmpty()){
+            this.contents.removeAllElements();
+        }
     }
 
-    private/protected/public String filename;
-    private/protected/public Vector<String> contents;
+    private String filename;
+    private Vector<String> contents;
 }
