66fc616194aa3f53237832e6c1a15c629a776370
ShingJan <yuanj.shi@hotmail.com>
2016-11-27 00:46:17 +0800

    assignment 5 init

---
+++ Assignment 5/src/BankAccount.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Isaac on 11/27/16.
+ */
+public class BankAccount {
+    private int balance;
+
+    public BankAccount(int balance){
+        this.balance = balance;
+    }
+
+    public void deposit(int amount){
+
+    }
+
+    public void withdraw(int amount){
+
+    }
+
+    public int getBalance(){
+        return balance;
+    }
+}
---
+++ Assignment 5/src/Depositor.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Isaac on 11/27/16.
+ */
+public class Depositor implements Runnable{
+    private BankAccount ba;
+    private int amount;
+    public Depositor(BankAccount ba, int amount){
+        this.ba = ba;
+        this.amount = amount;
+    }
+    public void run() {
+        for(int i = 0; i < 10; i++){
+            ba.deposit(amount); // A deposit is always allowed.
+        }
+    }
+}
