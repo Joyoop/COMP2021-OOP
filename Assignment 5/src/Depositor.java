944c7a6d9541d4c16cb672392968912cab8de33c
ShingJan <yuanj.shi@hotmail.com>
2016-11-27 18:22:24 +0800

    some changes but not finished yet

--- Assignment 5/src/Depositor.java
+++ Assignment 5/src/Depositor.java
@@ -11,6 +11,7 @@ public class Depositor implements Runnable{
     public void run() {
         for(int i = 0; i < 10; i++){
             ba.deposit(amount); // A deposit is always allowed.
+            System.out.print(ba.getBalance());
         }
     }
 }
