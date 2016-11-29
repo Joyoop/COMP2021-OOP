dd5963fcc75d5a25e1050a1c78ed48c51d370fed
ShingJan <yuanj.shi@hotmail.com>
2016-11-29 14:16:06 +0800

    delete Game class (useless)

--- monopoly/src/hk/edu/polyu/comp/comp2021/monopoly/Game.java
+++
@@ -1,8 +0,0 @@
-package hk.edu.polyu.comp.comp2021.monopoly;
-
-public class Game {
-
-    public static void main(String[] args){
-        System.out.println("This is a template for the Monopoly project.");
-    }
-}
